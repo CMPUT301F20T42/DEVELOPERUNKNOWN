df80130291e7a3784aa3fc99d9655eb63f5101a3
XZPshaw <zepeng@ualberta.ca>
2020-10-04 16:24:05 -0600

    new structure skeleton is built

--- app/src/main/java/com/example/developerunknown/CustomList.java
+++ app/src/main/java/com/example/developerunknown/CustomList.java
@@ -1,4 +1,16 @@
 package com.example.developerunknown;
 
+import android.content.Context;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+
 public class CustomList {
+
 }
+/*
+public class CustomList extends ArrayAdapter<Book> {
+        private ArrayList<Book> books;
+        private Context context;
+}
+*/
--- app/src/main/java/com/example/developerunknown/MainActivity.java
+++ app/src/main/java/com/example/developerunknown/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.developerunknown;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -31,8 +36,31 @@ public class MainActivity extends AppCompatActivity {
         startActivityForResult(intent,123);
 
         setContentView(R.layout.activity_main);
-
-
-
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,new HomeFragment()).commit();
     }
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+                    switch (item.getItemId()) {
+                        case R.id.nav_home:
+                            selectedFragment = new HomeFragment();
+                            break;
+                        case R.id.nav_search:
+                            selectedFragment = new SearchFragment();
+                            break;
+                        case R.id.nav_account:
+                            selectedFragment = new AccountFragment();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,selectedFragment).commit();
+
+                    return true;
+
+
+                    }
+                };
 }
