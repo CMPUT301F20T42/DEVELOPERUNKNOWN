cc696fb4f43f8dffbe75a8f0262615e700b4d7ea
AmazeFPig <haoyuan6@ualberta.ca>
2020-11-06 15:39:50 +0800

    Update AddBookFragment.java

--- app/src/main/java/com/example/developerunknown/AddBookFragment.java
+++ app/src/main/java/com/example/developerunknown/AddBookFragment.java
@@ -133,7 +133,9 @@ public class AddBookFragment extends Fragment {
         addBookButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                uploadImage();
+
+                Photographs.uploadImage("B", id, filePath, storageReference, applicationContext);
+
                 bookTitle = view.findViewById(R.id.book_title_editText);
                 bookAuthor = view.findViewById(R.id.book_author_editText);
                 bookStatus = view.findViewById(R.id.spinner);
@@ -281,51 +283,6 @@ public class AddBookFragment extends Fragment {
         }
 
 
-    // UploadImage method
-    private void uploadImage()
-    {
-
-        if (filePath != null) {
-
-            // Defining the child of storageReference
-            StorageReference ref = storageReference.child("BookImages/" + id);
-
-
-            // adding listeners on upload
-            // or failure of image
-            ref.putFile(filePath)
-                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                        @Override
-                        public void onSuccess(
-                                UploadTask.TaskSnapshot taskSnapshot)
-                        {
-                            // Image uploaded successfully
-                            Toast.makeText(applicationContext, "Image Uploaded!!", Toast.LENGTH_SHORT).show();
-                        }
-                    })
-
-                    .addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e)
-                        {
-                            // Error, Image not uploaded
-                            Toast.makeText(applicationContext, "Failed " + e.getMessage(), Toast.LENGTH_SHORT).show();
-                        }
-                    })
-                    .addOnProgressListener(new OnProgressListener<UploadTask.TaskSnapshot>() {
-
-                        // Progress Listener for loading
-                        // percentage on the dialog box
-                        @Override
-                        public void onProgress(UploadTask.TaskSnapshot taskSnapshot) {
-                            Toast.makeText(applicationContext, "In Progress ", Toast.LENGTH_SHORT).show();
-                        }
-                    });
-        }
-
-
-
-    }
 
 
     }
