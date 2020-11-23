bbcce45515590a534d0e0efa2034e72d02c2382e
gskeates <gskeates@ualberta.ca>
2020-11-22 20:25:19 -0700

    Removes book status spinner

--- app/src/main/java/com/example/developerunknown/EditBookFragment.java
+++ app/src/main/java/com/example/developerunknown/EditBookFragment.java
@@ -61,7 +61,6 @@ public class EditBookFragment extends Fragment {
 
     private EditText bookTitle;
     private EditText bookAuthor;
-    private Spinner bookStatus;
     private EditText bookDescription;
     private EditText bookISBN;
 
@@ -101,13 +100,11 @@ public class EditBookFragment extends Fragment {
 
         bookTitle = view.findViewById(R.id.book_title_editText);
         bookAuthor = view.findViewById(R.id.book_author_editText);
-        bookStatus = view.findViewById(R.id.spinner);
         bookDescription = view.findViewById(R.id.book_description_editText);
         bookISBN = view.findViewById(R.id.book_isbn_editText);
 
         bookTitle.setText(clickedBook.getTitle());
         bookAuthor.setText(clickedBook.getAuthor());
-        bookStatus.setSelection(((ArrayAdapter) bookStatus.getAdapter()).getPosition(clickedBook.getStatus()));
         bookDescription.setText(clickedBook.getDescription());
         bookISBN.setText(clickedBook.getISBN());
 
@@ -122,7 +119,6 @@ public class EditBookFragment extends Fragment {
                 Photographs.uploadImage("B", clickedBook.getID(), filePath, storageReference, applicationContext);
                 String title = bookTitle.getText().toString();
                 String author = bookAuthor.getText().toString();
-                String status = bookStatus.getSelectedItem().toString();
                 String description = bookDescription.getText().toString();
                 String ISBN = bookISBN.getText().toString();
 
@@ -130,7 +126,7 @@ public class EditBookFragment extends Fragment {
                     // Change book
                     clickedBook.setTitle(title);
                     clickedBook.setAuthor(author);
-                    clickedBook.setStatus(status);
+                    clickedBook.setStatus("Available");
                     clickedBook.setDescription(description);
                     clickedBook.setISBN(ISBN);
                     // Update database
@@ -138,7 +134,7 @@ public class EditBookFragment extends Fragment {
                     HashMap<String, String> data = new HashMap<>();
                     data.put("title", title);
                     data.put("author", author);
-                    data.put("status", status);
+                    data.put("status", "Available");
                     data.put("description", description);
                     // What if user updates the ISBN?
                     data.put("ISBN", ISBN);
